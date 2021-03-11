bae3cdf07a715d38f90fb5036a21c54ec908c9e8
Jeremias Nunez <jeremias2@gmail.com>
2021-03-11 00:01:42 -0400

    Added TailwindCSS and GraphQL

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,5 @@
+<script>
+  import './main.css';
+</script>
+
+<slot/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,47 @@
+<script context="module">
+	export const load = async({ fetch }) => {
+		const query = `
+			query Doubled($x: Int) {
+				double(number: $x)
+			}
+		`;
+
+		const variables = {
+			x: 19,
+		};
+
+		const response = await fetch("/graphql", {
+			body: { query, variables },
+			headers: {
+				"Authorization": "Token ABC123",
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+		});
+
+		const { data, errors } = await response.json();
+
+		if (errors) return {
+			error: new Error(errors.map(({ message }) => message).join("\n")),
+			status: 500,
+		};
+
+		return {
+			props: {
+				doubled: data.double,
+			},
+		};
+	}
+</script>
 <script>
 	import Counter from '$components/Counter.svelte';
+	export let doubled;
 </script>
 
 <main>
-	<h1>Hello world!  aver</h1>
+	<h1 class="uppercase text-5xl font-extrabold my-16">Hello world!</h1>
 
 	<Counter/>
 	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+	<p>Visit <a href="/graphql">GraphiQL</a> to explore the API.</p>
 </main>
